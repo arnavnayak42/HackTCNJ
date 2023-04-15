21f46b124e6c6f226d333acbcf1043dc7430bf0b
Arnav Nayak <arnavnayak@icloud.com>
2023-04-15 14:32:02 -0400

    the objects created

--- main.py
+++ main.py
@@ -1,84 +1,102 @@
 import pygame
 import random
 
+# Constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+BG_COLOR = (0, 0, 0)  # Black
+PLAYER_COLOR = (0, 255, 0)  # Green
+ENEMY_COLOR = (255, 0, 0)  # Red
+DOOR_COLOR = (0, 0, 255)  # Blue
+
+# Maze
+maze = [
+    "##########",
+    "#  #  #  #",
+    "#  #  #  #",
+    "#        #",
+    "#  #  #  #",
+    "#  #  #  #",
+    "#        #",
+    "#  #  #  #",
+    "#  #  #  #",
+    "##########",
+]
+
+# Trivia questions and answers
+trivia_questions = {
+    "What is the capital of France?": "Paris",
+    "What is the largest mammal on Earth?": "Blue whale",
+    "What is the chemical symbol for gold?": "Au",
+    "What is the tallest mountain in the world?": "Mount Everest",
+}
+
 # Initialize Pygame
 pygame.init()
-
-# Set up display
-width, height = 640, 480
-screen = pygame.display.set_mode((width, height))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Maze Game")
 
-# Set up colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
+# Player class
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((20, 20))
+        self.image.fill(PLAYER_COLOR)
+        self.rect = self.image.get_rect()
+        self.rect.x = 20
+        self.rect.y = 20
 
-# Set up player character
-player_pos = [30, 30]
-player_size = 20
+    def update(self, walls):
+        # Check for collision with walls
+        hits = pygame.sprite.spritecollide(self, walls, False)
+        for hit in hits:
+            if self.rect.colliderect(hit.rect):
+                if self.rect.right > hit.rect.left and self.rect.left < hit.rect.left:
+                    self.rect.right = hit.rect.left
+                elif self.rect.left < hit.rect.right and self.rect.right > hit.rect.right:
+                    self.rect.left = hit.rect.right
+                elif self.rect.bottom > hit.rect.top and self.rect.top < hit.rect.top:
+                    self.rect.bottom = hit.rect.top
+                elif self.rect.top < hit.rect.bottom and self.rect.bottom > hit.rect.bottom:
+                    self.rect.top = hit.rect.bottom
 
-# Set up end point
-end_pos = [width - 30, height - 30]
-end_size = 20
+# Enemy class
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((20, 20))
+        self.image.fill(ENEMY_COLOR)
+        self.rect = self.image.get_rect()
+        self.rect.x = SCREEN_WIDTH - 40
+        self.rect.y = SCREEN_HEIGHT - 40
 
-# Set up walls
-walls = []
-for i in range(10):
-    wall_x = random.randint(0, width - 20)
-    wall_y = random.randint(0, height - 20)
-    wall_width = random.randint(50, 150)
-    wall_height = random.randint(50, 150)
-    walls.append(pygame.Rect(wall_x, wall_y, wall_width, wall_height))
+    def update(self):
+        # Move enemy randomly
+        direction = random.choice(["left", "right", "up", "down"])
+        if direction == "left":
+            self.rect.x -= 2
+        elif direction == "right":
+            self.rect.x += 2
+        elif direction == "up":
+            self.rect.y -= 2
+        elif direction == "down":
+            self.rect.y += 2
 
-# Main game loop
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # Get player input
-    keys = pygame.key.get_pressed()
-    
-    if pygame.KEYDOWN:
-        if keys[pygame.K_w]:
-            player_pos[1] -= .3
-        if keys[pygame.K_s]:
-            player_pos[1] += .3
-        if keys[pygame.K_a]:
-            player_pos[0] -= .3
-        if keys[pygame.K_d]:
-            player_pos[0] += .3
-
-    # Check collision with walls
-    player_rect = pygame.Rect(player_pos[0], player_pos[1], player_size, player_size)
-    for wall in walls:
-        if player_rect.colliderect(wall):
-            player_pos = [30, 30]  # Reset player position if collision occurs
-
-    # Check if player reached the end point
-    end_rect = pygame.Rect(end_pos[0], end_pos[1], end_size, end_size)
-    if player_rect.colliderect(end_rect):
-        print("You won!")
-        running = False
-
-    # Draw background
-    screen.fill(BLACK)
-
-    # Draw walls
-    for wall in walls:
-        pygame.draw.rect(screen, GREEN, wall)
-
-    # Draw player character
-    pygame.draw.rect(screen, WHITE, player_rect)
-
-    # Draw end point
-    pygame.draw.rect(screen, RED, end_rect)
-
-    # Update display
-    pygame.display.flip()
-
-# Quit Pygame
-pygame.quit()
+# Door class
+class Door(pygame.sprite.Sprite):
+    def __init__(self, trivia_question, answer):
+        super().__init__()
+        self.image = pygame.Surface((20, 20))
+        self.image.fill(DOOR_COLOR)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(40, SCREEN_WIDTH - 60)
+        trivia_question = {
+            "What is the capital of France": "Paris",
+            "How tall is Lebron James": "6'7",
+            "Which country has the highest population": "China"
+        }
+print(trivia_questions[1])
+#Create sprites
+player = Player()
+enemy = Enemy()
+door = Door("")
